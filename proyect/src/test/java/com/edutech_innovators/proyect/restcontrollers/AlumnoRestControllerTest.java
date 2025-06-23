b91174a5af6bfd4ca0d0d4a47b53d0abb86cfc4e
nicolas <nico.carrascor@duocuc.cl>
2025-06-23 14:15:23 -0400

    fix bug

--- proyect/src/test/java/com/edutech_innovators/proyect/restcontrollers/AlumnoRestControllerTest.java
+++ proyect/src/test/java/com/edutech_innovators/proyect/restcontrollers/AlumnoRestControllerTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import com.edutech_innovators.proyect.entities.Alumno;
 import com.edutech_innovators.proyect.services.AlumnoServiceImpl;
@@ -30,7 +30,7 @@ public class AlumnoRestControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private AlumnoServiceImpl service;
 
     @Autowired 
